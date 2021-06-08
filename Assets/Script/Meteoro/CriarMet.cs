6f71302c3e91de11b3591f4f1b0987d4fd683a30
Lucas Felipe Ferreira <capitaorox9@gmail.com>
2021-06-07 22:35:55 -0300

    07/06**
    Alguns bug consertados

--- Assets/Script/Meteoro/CriarMet.cs
+++ Assets/Script/Meteoro/CriarMet.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class CriarMet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector3 posicao;
 
-    // Update is called once per frame
     void Update()
     {
         
